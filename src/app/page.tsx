45f36aa745e5e914ecb694bb0527b3831f06372e
liam <162596678+liamdidthat@users.noreply.github.com>
2026-06-07 03:38:34 +0100

    Implemented basic elements, content & page structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,40 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    <main className="bg-zinc-950">
+
+      <title>croaker</title>
+
+      <h1 className="text-6xl font-bold font-mono text-center">Liam Hughes</h1>
+      <br></br>
+
+      <h2 className="text-4xl font-bold font-mono">About</h2>
+      <br></br>
+
+      <h3>Hey, I'm Liam.</h3>
+      <p className="font-mono">I'm a Software Engineer, currently in my first year, studying BSc Computer Science at the University of Kent. <br></br> 
+        Alongside my studies, I am working on <b><i>NSMB+</i></b> a rework of the flagship nDS game; "New. Super Mario Bros. DS"<br></br>
+        To supplement this mod project, I created a .nds audio extractor/convertor: <b><i>nDS Audio Toolkit</i></b> — learn more <a href="https://github.com/Liam-GH/nDSAudioToolkit"><i><b>here</b></i> </a> <br></br>
+        <br></br>
+
+        Currently, I work as an AI Data Trainer at Outlier AI, which ensures i'm kept up to speed with the booming AI industry.<br></br>
+        On top of my current job, I'll be attending BrightNetwork's IEUK2026, as a Technology & Engineering Intern. <br></br>
+        </p>
+
+        <br></br>
+    
+        <h2 className="text-4xl font-bold font-mono">Projects</h2>
+
+
+        <h2 className="text-4xl font-bold font-mono">Experience</h2>
+
+
+        <h2 className="text-4xl font-bold font-mono">Blog</h2>
+
+
+
+    
+    
+    </main>
+  )
 }
+
